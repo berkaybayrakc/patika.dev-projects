b67ffd0c53756cf7b83100b76d5823c78b3cbba4
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2023-01-01 15:33:50 +0300

    Not Ortalaması Hesaplama

---
+++ Java101Projects/CalculateAverage.java
@@ -0,0 +1,36 @@
+package Java101Projects;
+
+import java.util.Scanner;
+
+public class CalculateAverage {
+    public static void main(String[] args) {
+
+        int mat,fizik,tarih,muzik,turkce,kimya;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz: ");
+        mat = scanner.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz: ");
+        fizik = scanner.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz: ");
+        tarih = scanner.nextInt();
+
+        System.out.println("Muzik notunuzu giriniz: ");
+        muzik = scanner.nextInt();
+
+        System.out.println("Turkce notunuzu giriniz: ");
+        turkce = scanner.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz: ");
+        kimya = scanner.nextInt();
+
+        int ort = mat+kimya+fizik+muzik+tarih+turkce;
+
+        System.out.println(ort>60 ? "Sinifi Gecti":"Sınıfta Kaldı");
+
+    }
+
+}
